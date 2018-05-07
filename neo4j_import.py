391a6f6790b6bfa5039b2e173a82cffb983d5647
Simon Hohl <simon.hohl@dainst.org>
2018-05-07 11:22:15 +0200

    Split person inserts into separate function

--- neo4j_import.py
+++ neo4j_import.py
@@ -1,22 +1,55 @@
 import sys
+import logging
 import re
 
 from neo4j.v1 import GraphDatabase
 
 PAGE_COUNT_PATTERN = re.compile('.*(\d+)\s*Seiten.*')
+DATE_PATTERN = re.compile('\d{4}-\d{2}-\d{2}')
 
 
 def _extract_locations(line_values):
-    result = {
-        'from': {'name': line_values[5].rstrip('.'), 'gazetteer_id': line_values[6]},
-        'to': {'name': line_values[15].rstrip('.'), 'gazetteer_id': line_values[16]}
-    }
-
+    result = dict()
+    if line_values[5] != '' or line_values[6] != '':
+        result['from'] = {'name': line_values[5].rstrip('.'), 'gazetteer_id': line_values[6]}
+    else:
+        result['from'] = None
+    if line_values[15] != '' or line_values[16]:
+        result['to'] = {'name': line_values[15].rstrip('.'), 'gazetteer_id': line_values[16]}
+    else:
+        result['to'] = None
     return result
 
 
+def _compare_locations(a, b):
+    if a is None and b is not None:
+        return False
+    elif a is not None and b is None:
+        return False
+    elif a is None and b is None:
+        return True
+
+    if a['name'] == b['name']:
+        return True
+        # if 'gazetteer_id' in a and 'gazetteer_id' in b:
+        #     if a['gazetteer_id'] == b['gazetteer_id']:
+        #         return True
+        #     else:
+        #         return False
+        # elif 'gazetteer_id' not in a and 'gazetteer_id' not in b:
+        #     return True
+        # else:
+        #     return False
+    else:
+        return False
+
+
 def _extract_date(line_values):
-    return line_values[7]
+    match = DATE_PATTERN.match(line_values[7])
+    if match is not None:
+        return match.group(0)
+    else:
+        return ''
 
 
 def _extract_persons(line_values):
@@ -56,63 +89,175 @@ def _parse_page_count( value):
         return -1
 
 
+def _create_localization(date, location):
+
+    localisations = [{
+        'location': location,
+        'from': date,
+        'until': date
+    }]
+    return localisations
+
+
+def _import_persons(session, lines):
+    persons = dict()
+    localizations = dict()
+    space_time_points = dict()
+
+    for line_values in lines:
+
+        date = _extract_date(line_values)
+        locations = _extract_locations(line_values)
+
+        extracted_data = _extract_persons(line_values)
+
+        if locations['from'] is not None:
+            for person in extracted_data['authors']:
+                values = tuple(person.values())
+                if values not in space_time_points:
+                    space_time_points[values] = [(date, locations['from']['name'], locations['from']['gazetteer_id'])]
+                else:
+                    space_time_points[values] += [(date, locations['from']['name'], locations['from']['gazetteer_id'])]
+
+        if locations['to'] is not None:
+            for person in extracted_data['recipients']:
+                values = tuple(person.values())
+                if values not in space_time_points:
+                    space_time_points[values] = [(date, locations['to']['name'], locations['to']['gazetteer_id'])]
+                else:
+                    space_time_points[values] += [(date, locations['to']['name'], locations['to']['gazetteer_id'])]
+
+    for person_index in space_time_points:
+        # if person_index == ('Gerhard, Eduard', '118717030'):
+        #     print(space_time_points[person_index])
+        space_time_points[person_index] = set(space_time_points[person_index])
+        space_time_points[person_index] = sorted(space_time_points[person_index], key=lambda x: (x[0], x[1]))
+        space_time_points[person_index] = [{'date': x[0], 'location':{'name': x[1], 'gazetteer_id': x[2]}} for x in space_time_points[person_index]]
+        # if person_index == ('Gerhard, Eduard', '118717030'):
+        #     print(space_time_points[person_index])
+
+        current_localization = []
+        current_location = None
+        current_from_time = None
+        current_until_time = None
+        for current_point in space_time_points[person_index]:
+            if not _compare_locations(current_location, current_point['location']):
+
+                # if person_index == ('Gerhard, Eduard', '118717030'):
+                #     print('new location in space time points. final from: %s and until: %s' % (current_from_time, current_until_time))
+                #     print('final location:')
+                #     print(current_location)
+                #     print(current_point['location'])
+
+                if current_location is not None:
+                    current_localization.append({
+                        'location': current_location,
+                        'from': current_from_time,
+                        'to': current_until_time
+                    })
+
+                current_location = current_point['location']
+                current_from_time = current_point['date']
+                current_until_time = current_point['date']
+            else:
+                current_until_time = current_point['date']
+
+        current_localization.append({
+            'location': current_location,
+            'from': current_from_time,
+            'to': current_until_time
+        })
+
+        localizations[person_index] = current_localization
+
+    for line_values in lines:
+        extracted_data = _extract_persons(line_values)
+        for person in extracted_data['authors']:
+            values = tuple(person.values())
+
+            if values in localizations:
+                person['localization'] = localizations[values]
+
+            if values not in persons:
+                persons[values] = person
+
+        for person in extracted_data['recipients']:
+            values = tuple(person.values())
+
+            if values in localizations:
+                person['localization'] = localizations[values]
+
+            if values not in persons:
+                persons[values] = person
+
+    with session.begin_transaction() as tx:
+        for person in list(persons.values()):
+            if 'localization' in person:
+                for localization in person['localization']:
+
+                    create_statement = \
+                        'MERGE (location:Place {name:{place_name}, gazetteer_id:{gazetteer_id}})' \
+                        'MERGE (l:Localisation{from: {from}, until: {until}})' \
+                        '-[:HAS_PLACE]->(location)-[:DEFINES]->(l)' \
+                        'MERGE (p:Person{name:{person}.name, gnd_id:{person}.gnd_id})' \
+                        'MERGE (p)-[:IS_LOCATED]->(l)-[:LOCATES]->(p)'
+
+                    tx.run(create_statement,
+                           {
+                               'place_name': localization['location']['name'],
+                               'gazetteer_id': localization['location']['gazetteer_id'],
+                               'from': localization['from'],
+                               'until': localization['to'],
+                               'person': person
+                           })
+            else:
+                create_statement = \
+                    'MERGE (p:Person{name:{person}.name, gnd_id:{person}.gnd_id})' \
+
+                tx.run(create_statement,
+                       {
+                           'person': person
+                       })
+
+
 def import_data(tsv_path, uri, user, password, ignore_first_line):
     driver = GraphDatabase.driver(uri, auth=(user, password))
     with driver.session() as session, open(tsv_path, 'r') as input_file:
 
-        counter = 0
+        lines = []
         for line in input_file:
             if ignore_first_line:
                 ignore_first_line = False
                 continue
 
             line_values = line.split('\t')
+            lines.append(line_values)
 
-            locations = _extract_locations(line_values)
+
+
+        print('Importing persons...')
+        _import_persons(session, lines)
+        print('Importing letters...')
+        counter = 0
+        for line_values in lines:
             persons = _extract_persons(line_values)
             letter = _extract_letter(line_values)
 
-            localisation_statement = \
-                'MERGE (location:Location {name:{name}, gazetteer_id:{gazetteer_id}})' \
-                'MERGE (date:Date{date:{date}})' \
-                'MERGE (location)-[:DEFINES]->(l:Localisation)<-[:DEFINES]-(date)' \
-                'MERGE (date)<-[:IS_DEFINED_BY_DATE]-(l)-[:IS_DEFINED_BY_LOCATION]->(location)'
-
             letter_statement = \
-                'MATCH (date:Date{date:{date}})' \
-                'CREATE (letter:Letter{title:{title}, desc:{desc}, quant:{quant}, summ:{summ}, id:{id}})' \
-                'CREATE (date)-[:DATES]->(letter)-[:WAS_WRITTEN_DATE]->(date)'
-
-            person_statement = \
-                'MATCH (location:Location {name:{location_name}, gazetteer_id:{gazetteer_id}})' \
-                'MATCH (date:Date{date:{date}})' \
-                'MATCH (location)-[:DEFINES]->(l:Localisation)<-[DEFINES]-(date)' \
-                'MERGE (person:Person {name:{name}, gnd_id:{gnd_id}})' \
-                'MERGE (l)-[:LOCALIZES]->(person)-[:IS_LOCALIZED_BY]->(l)' \
+                'CREATE (letter:Letter{' \
+                'title:{title}, quantity_desc:{desc}, page_count:{quant}, summ:{summ}, id:{id}, date:{date}})' \
 
             link_author_statement \
-                = 'MATCH (letter:Letter{title:{title}, desc:{desc}, quant:{quant}, summ:{summ}, id:{id}})' \
+                = 'MATCH (letter:Letter{id:{id}})' \
                   'MATCH (person:Person {name:{name}, gnd_id:{gnd_id}})' \
                   'CREATE (person)-[:IS_AUTHOR]->(letter)-[:IS_AUTHERED_BY]->(person)'
 
             link_recipient_statement \
-                = 'MATCH (letter:Letter{title:{title}, desc:{desc}, quant:{quant}, summ:{summ}, id:{id}})' \
+                = 'MATCH (letter:Letter{id:{id}})' \
                   'MATCH (person:Person {name:{name}, gnd_id:{gnd_id}})' \
                   'CREATE (person)-[:IS_RECIPIENT_OF]->(letter)-[:IS_RECEIVED_BY]->(person)'
 
             with session.begin_transaction() as tx:
-                tx.run(localisation_statement,
-                       {
-                           'date': line_values[7],
-                           'name': locations['from']['name'],
-                           'gazetteer_id': locations['from']['gazetteer_id']
-                       })
-                tx.run(localisation_statement,
-                       {
-                           'date': line_values[7],
-                           'name': locations['to']['name'],
-                           'gazetteer_id': locations['to']['gazetteer_id']
-                       })
 
                 tx.run(letter_statement,
                        {
@@ -123,17 +268,7 @@ def import_data(tsv_path, uri, user, password, ignore_first_line):
                            'summ': letter['summary'],
                            'id': counter
                        })
-
                 for person in persons['authors']:
-                    tx.run(person_statement,
-                           {
-                               'date': line_values[7],
-                               'location_name': locations['from']['name'],
-                               'gazetteer_id': locations['from']['gazetteer_id'],
-                               'name': person['name'],
-                               'gnd_id': person['gnd_id']
-                           })
-
                     tx.run(link_author_statement,
                            {
                                'title': letter['title'],
@@ -145,16 +280,7 @@ def import_data(tsv_path, uri, user, password, ignore_first_line):
                                'gnd_id': person['gnd_id'],
                                'id': counter
                            })
-
                 for person in persons['recipients']:
-                    tx.run(person_statement,
-                           {
-                               'date': line_values[7],
-                               'location_name': locations['to']['name'],
-                               'gazetteer_id': locations['to']['gazetteer_id'],
-                               'name': person['name'],
-                               'gnd_id': person['gnd_id']
-                           })
 
                     tx.run(link_recipient_statement,
                            {
