82881173d7102f19dfd910f901c0f6d1d279e28c
Simon Hohl <simon.hohl@dainst.org>
2018-06-18 15:23:10 +0200

    Add EAD file parsing to main import script

--- graph_db_imports/import.py
+++ graph_db_imports/import.py
@@ -3,7 +3,7 @@ import logging
 import os
 
 from tsv_reader import read_data as read_tsv_data
-from ead_reader import read_ead_file
+from ead_reader import read_ead_file, read_ead_files
 from neo4j_writer import write_data
 
 logging.basicConfig(format='%(asctime)s %(message)s')
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     if len(sys.argv) != 6:
         logger.info('Please provide as arguments: ')
 
-        logger.info('1) The TSV file containing metadata.')
+        logger.info('1) Directory or file containing the metadata files (TSV or EAD XML).')
         logger.info('2) Neo4j URL')
         logger.info('3) Neo4j port')
         logger.info('4) Neo4j username')
@@ -24,14 +24,35 @@ if __name__ == '__main__':
 
         sys.exit()
 
-    [file_name, file_extension] = os.path.splitext(sys.argv[1])
+    input_path = sys.argv[1]
 
-    if file_extension == '.tsv':
-        letter_data = read_tsv_data(tsv_path=sys.argv[1], ignore_first_line=True)
-    elif file_extension == '.xml':
-        letter_data = read_ead_file(ead_file=sys.argv[1])
+    if os.path.isfile(input_path):
+        [file_name, file_extension] = os.path.splitext(input_path)
+
+        if file_extension == '.tsv':
+            letter_data = read_tsv_data(tsv_path=input_path, ignore_first_line=True)
+        elif file_extension == '.xml':
+            letter_data = read_ead_file(ead_file=input_path)
+        else:
+            logger.warning(f'Not a valid file format: {input_path}')
+            sys.exit()
+    elif os.path.isdir(input_path):
+
+        if not input_path.endswith('/'):
+            input_path += '/'
+
+        files_in_dir = [f for f in os.listdir(input_path) if os.path.isfile(os.path.join(input_path, f))]
+
+        xml_files_in_dir = [f'{input_path}{f}' for f in files_in_dir if os.path.splitext(f)[1] == '.xml']
+
+        if len(xml_files_in_dir) != 0:
+            logger.debug(xml_files_in_dir)
+            letter_data = read_ead_files(xml_files_in_dir)
+        else:
+            logger.warning(f'Not valid files found in directory: {input_path}')
+            sys.exit()
     else:
-        logger.warning(f'Not a valid file format: {sys.argv[1]}')
+        logger.warning(f'No valid files found at {input_path}.')
         sys.exit()
 
     write_data(letter_data, url=sys.argv[2], port=int(sys.argv[3]), username=sys.argv[4], password=sys.argv[5])
